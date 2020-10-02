610b855570aa96f88249442b4c14b3c344ff3f99
burnt776 <burnt776@gmail.com>
2020-10-02 11:24:19 +0530

    performing calculations

---
+++ JavaScript/01Basic/celcius.js
@@ -0,0 +1,15 @@
+var score = 100
+
+score = 130
+
+const bonus = 50
+
+var finalScore = (score + bonus) * 0.8
+
+var complex = ((4 + 4) * 5) / 2
+
+
+
+
+// console.log(finalScore)
+// console.log(complex)
