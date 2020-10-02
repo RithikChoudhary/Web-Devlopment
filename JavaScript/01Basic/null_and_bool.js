0991c27912003938daacbbba18af2f75e93fd27c
burnt776 <burnt776@gmail.com>
2020-10-02 11:34:46 +0530

    Boolean and undefiend variable

---
+++ JavaScript/01Basic/null_and_bool.js
@@ -0,0 +1,11 @@
+var temp
+//will be showing undefined bcoz the temp does not have any value
+console.log('current temperature is : ' + temp)
+
+
+//Boolean = True/False
+
+let actualMarks = 7
+let myGrade = (actualMarks < 10)
+
+console.log(myGrade)
