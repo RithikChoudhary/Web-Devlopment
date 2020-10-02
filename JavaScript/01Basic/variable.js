012e6a6290f6a09b1cc22c14bd788658ca706d78
burnt776 <burnt776@gmail.com>
2020-10-02 11:17:11 +0530

    Variable and const

---
+++ JavaScript/01Basic/variable.js
@@ -0,0 +1,16 @@
+// declaring variable and its value 
+let name="John"
+
+let score= 183
+let bonus = 20
+let total = score + bonus
+
+console.log(total)
+
+const FirstName = 'John'
+let lastName = 'Doe'
+let fullName = FirstName + ' ' +  lastName
+
+// console.log(FirstName + ' ' + lastName)
+console.log(fullName)
+
