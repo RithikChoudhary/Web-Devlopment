5572b05e3c46c0f5aa6d8b528b819305df90a807
burnt776 <burnt776@gmail.com>
2020-10-02 11:27:19 +0530

    fahrenheit and celsius conversion

--- JavaScript/01Basic/celcius.js
+++ JavaScript/01Basic/celcius.js
@@ -1,15 +1,24 @@
-var score = 100
+// var score = 100
 
-score = 130
+// score = 130
 
-const bonus = 50
+// const bonus = 50
 
-var finalScore = (score + bonus) * 0.8
+// var finalScore = (score + bonus) * 0.8
 
-var complex = ((4 + 4) * 5) / 2
+// var complex = ((4 + 4) * 5) / 2
 
 
 
 
 // console.log(finalScore)
 // console.log(complex)
+
+
+let tempFahrenheit = 32
+
+//do calculation
+
+let celsius = (tempFahrenheit - 32) * 5/9
+
+console.log('The temperature in celsius = ' + celsius)
