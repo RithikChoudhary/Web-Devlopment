2e9394a9d739b891a80e9dfeab5a0441d8fc30c3
burnt776 <burnt776@gmail.com>
2020-10-04 01:33:14 +0530

    operator usage

---
+++ JavaScript/02Basic/operators.js
@@ -0,0 +1,17 @@
+let isVerified = true
+let isLoggedIn = true
+let hasPaymentToken = true
+let isGuest = true
+if ( isVerified && isLoggedIn && hasPaymentToken)
+{
+    console.log('Greeting message to user')
+    console.log('Grant access to paid course')
+}
+else if (isVerified || isGuest)
+{
+    console.log('Allow free previews')
+}
+else
+{
+    console.log('Message to admin for query')
+}
