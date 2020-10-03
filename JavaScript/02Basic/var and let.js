cea0fcf1afca4fed9b26841531a172a2d38ab61d
burnt776 <burnt776@gmail.com>
2020-10-04 01:37:57 +0530

    decalring loacal and global variable

--- JavaScript/02Basic/var and let.js
+++ JavaScript/02Basic/var and let.js
@@ -1,5 +1,15 @@
-let imGlobal = 'someValue'
-if(true)
+// let imGlobal = 'someValue'
+// if(true)
+// {
+//     let imLocal = 'someMoreValue'
+//     console.log(imGlobal)
+//     console.log(imLocal)
+// }
+
+var x = 10;
+// Here x is 10
 {
-    console.log('im Global here')
+  let x = 2;
+  // Here x is 2
 }
+// Here x is 10 
