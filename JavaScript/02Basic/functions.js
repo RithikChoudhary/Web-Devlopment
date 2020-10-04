f6ff2630f90c05557f875401840768cca6c74cff
burnt776 <burnt776@gmail.com>
2020-10-04 22:21:33 +0530

    functions

---
+++ JavaScript/02Basic/functions.js
@@ -0,0 +1,18 @@
+// //Here we define the function to a variable 
+// let sayHello = function (name) {
+//     console.log('Greeting message for user');
+//     console.log(`Hey ${name}`);
+// }
+// console.log(sayHello)
+
+// //running the function 
+// sayHello('John')
+
+
+let fullNameMaker = function (firstName, lastName) {
+    console.log('Welcoem to LCo')
+    console.log(`Happy to have you , ${firstName} ${lastName}`)
+}
+
+fullNameMaker('Rithik', 'Choudhary')
+
