521c977612bc4034d27562da64c2b6ee025ffb2d
burnt776 <burnt776@gmail.com>
2020-10-04 22:09:38 +0530

    Arrays

---
+++ JavaScript/02Basic/Array.js
@@ -0,0 +1,16 @@
+console.log();
+
+// console.log(superHeros[1]);
+const superHeros = ['Iron Man', 'Spiderman', 'Capt', 'America', 'Thor']
+console.log(superHeros[1]);
+
+console.log(superHeros);
+console.log(superHeros[1]);
+console.log(superHeros[5]);
+
+//calculate the length of array
+console.log(superHeros.length)
+console.log(superHeros[superHeros.length -1])
+
+//
+console.log(`We have ${superHeros.length} super heros`)
