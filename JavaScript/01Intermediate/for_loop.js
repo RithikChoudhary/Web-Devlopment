99d92f4267cb9183e5f923f32e2c93640326a79d
burnt776 <burnt776@gmail.com>
2020-10-04 22:52:20 +0530

    for loop

---
+++ JavaScript/01Intermediate/for_loop.js
@@ -0,0 +1,11 @@
+//for loop
+
+const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+for (let index = 0; index < days.length; index++) {
+
+    const element = days[index];
+
+    console.log(element)
+
+}
