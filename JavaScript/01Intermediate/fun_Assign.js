8bf9a4219463e4a234100bd5fe8907d933efba26
burnt776 <burnt776@gmail.com>
2020-10-06 00:46:38 +0530

    fun task

---
+++ JavaScript/01Intermediate/fun_Assign.js
@@ -0,0 +1,9 @@
+const mytodo = []
+
+mytodo.push('But Bread')
+mytodo.push('Record videos for youtube')
+mytodo.push('Go to gym')
+
+mytodo.forEach(function(todo,index){
+    console.log(`Your task no. ${index +1} is: ${todo}`)
+})
