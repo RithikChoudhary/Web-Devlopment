c8381ba71a4c51190900eca3ae217427a01f12a3
burnt776 <burnt776@gmail.com>
2020-10-06 01:30:55 +0530

    objects :)

---
+++ JavaScript/02Intermediate/objects.js
@@ -0,0 +1,15 @@
+
+//object declaration
+let myYoutube = {
+    title: 'Loops in javaScript',
+    videoLength: '15',
+    videoCreator: 'Rithik Choudhary',
+    videoDescription: 'THis is video description and a long one',
+
+}
+
+//accessing the object myYoutube
+// console.log(myYoutube);
+
+console.log(`Hey new Video on ${myYoutube.title} by ${myYoutube.videoCreator}`)
+
