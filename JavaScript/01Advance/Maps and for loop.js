69002b3418c1a2ae014447df4283e2452118d9f2
burnt776 <burnt776@gmail.com>
2020-10-07 19:56:21 +0530

    Map in js

---
+++ JavaScript/01Advance/Maps and for loop.js
@@ -0,0 +1,21 @@
+var john = {
+    name: 'I am john',
+    age: 24,
+    isActive: true
+}
+
+var marry = {
+    name: 'I am marry',
+    age: 23,
+    isActive: true,
+}
+
+var sam = {
+    name: 'I am sam',
+    age: 29,
+    isActive: false
+}
+
+let users = new Map()
+
+console.log(typeof users);
