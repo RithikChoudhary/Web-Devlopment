c909f8002c40a9985d5d5107e51d284497bc735c
burnt776 <burnt776@gmail.com>
2020-10-09 23:49:47 +0530

    form validation

--- Web Basic practice/set 4/validation.js
+++ Web Basic practice/set 4/validation.js
@@ -1,17 +1,28 @@
 // alert('connected')
-function myValidation(){
+function myValidation() {
     // alert('hey connect')
     // let myValue = document.querySelector('myform')
     let myValue = document.getElementById('myform').value;
 
     //isNaN is to check that it is not number
-    if (isNaN(myValue) || myValue < 1 || myValue >20) {
+    if (isNaN(myValue) || myValue < 1 || myValue > 20) {
         console.log("Not a valid input")
     }
-    else{
+    else {
         console.log("This input is cool");
     }
 
 
 
 }
+
+//form validation
+// document.getElementsByClassName('myform')
+document.querySelector('.myform').addEventListener('submit', (event) => {
+    //preventDefault will stop browser to do any thing by it self
+    event.preventDefault();
+    console.log(event.target.username.value);
+    console.log(event.target.realname.value);
+    //replacing the username with blank
+    event.target.username.value = '';
+})
