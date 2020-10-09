11dfe54be46a796c63b0109f737f5026e139a058
burnt776 <burnt776@gmail.com>
2020-10-09 23:17:24 +0530

    query selection

---
+++ Web Basic practice/set 3/myscript.js
@@ -0,0 +1,14 @@
+//by this line it will show the title in the console of inspect
+// console.log(document.title);
+
+// document.title  = 'I get changed'
+
+//this command will get the paragraph with id
+// console.log(document.getElementById('idone'))
+
+// console.log(document.getElementsByClassName('classone'))
+
+// it will select all the paragraph
+const myElement = document.querySelectorAll('p')
+console.log(myElement)
+
