a4e25a98987f56160ab1d5887ba81855d90a20d7
burnt776 <burnt776@gmail.com>
2020-10-10 11:35:52 +0530

    create tags in js

--- 01Project/todo.js
+++ 01Project/todo.js
@@ -1,1 +1,5 @@
 // alert('connected')
+var h = document.createElement('h1')
+var myValue = document.createTextNode('Hello world')
+h.appendChild(myValue);
+document.querySelector('h1').appendChild(h)
