1ffb8dd966e842116dd2dc981f398556cfcbe797
burnt776 <burnt776@gmail.com>
2020-10-12 22:26:14 +0530

    change

---
+++ Projects/01Project/todo.js
@@ -0,0 +1,12 @@
+// // alert('connected')
+// var h = document.createElement('h1')
+// var myValue = document.createTextNode('Hello world')
+// h.appendChild(myValue);
+// document.querySelector('h1').appendChild(h)
+
+// var val = 5;
+// while(val > 0){
+//     console.log(val)
+//     val--;
+// }
+
