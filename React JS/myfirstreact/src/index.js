de71517b45d1727000d1f41d139d729027f1e483
burnt776 <burnt776@gmail.com>
2020-10-19 00:06:37 +0530

    mentioning more than one tag

--- React JS/myfirstreact/src/index.js
+++ React JS/myfirstreact/src/index.js
@@ -1,5 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>Rithik CHoudhary</h1>, document.getElementById('root'))
+ReactDOM.render(
+    <div>
+    <h1>Rithik CHoudhary</h1>
+    <p>Lets begin </p>
+    <h2>so we mention it in a div tag</h2>
+    </div>, 
+    
+    document.getElementById('root')
+);
+
  
