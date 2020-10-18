c963e532c312bc6b415db35dc23222e100153e7c
burnt776 <burnt776@gmail.com>
2020-10-18 22:11:09 +0530

    cb

--- React JS/hello-world/src/index.js
+++ React JS/hello-world/src/index.js
@@ -1,6 +1,7 @@
-var React = require('react');
-
-var ReactDOM = require('react-dom');
+// var React = require('react');
+import React from 'react'; //in this we get babel compiler
+// var ReactDOM = require('react-dom');
+import ReactDOM from 'react-dom'; // this is from dom module in browser
 
 // ReactDOM.render('kya dikhana ha' , 'kaha dikhana ha' , 'callback function');
 ReactDOM.render( <h1> Hello World! </h1> , 
