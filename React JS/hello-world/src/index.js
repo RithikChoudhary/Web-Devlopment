049333e2b5a516ac5efebb8a0256692676ab18b8
burnt776 <burnt776@gmail.com>
2020-10-18 22:14:40 +0530

    hello world with libraries

--- React JS/hello-world/src/index.js
+++ React JS/hello-world/src/index.js
@@ -6,3 +6,5 @@ import ReactDOM from 'react-dom'; // this is from dom module in browser
 // ReactDOM.render('kya dikhana ha' , 'kaha dikhana ha' , 'callback function');
 ReactDOM.render( <h1> Hello World! </h1> , 
   document.getElementById('root'));
+
+  
