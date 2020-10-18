b3acf6731b2b0eb6bc17cbfd6874af578f4a852c
burnt776 <burnt776@gmail.com>
2020-10-19 00:25:54 +0530

    make a ordered list in react

--- React JS/myfirstreact/src/index.js
+++ React JS/myfirstreact/src/index.js
@@ -14,26 +14,43 @@ import ReactDOM from 'react-dom'
 //     document.getElementById('root')
 // );
 
-ReactDOM.render(
-    <React.Fragment>
-    <div>
-    <h1>Rithik CHoudhary</h1>
-    <p>Lets begin </p>
-    <h2>so we mention it in a div tag</h2>
-    </div>
-    </React.Fragment>, 
+// ReactDOM.render(
+//     <React.Fragment>
+//     <div>
+//     <h1>Rithik CHoudhary</h1>
+//     <p>Lets begin </p>
+//     <h2>so we mention it in a div tag</h2>
+//     </div>
+//     </React.Fragment>, 
 
-    document.getElementById('root')
-);
+//     document.getElementById('root')
+// );
+
+// ReactDOM.render(
+//     <>
+//     <div>
+//     <h1>Rithik CHoudhary</h1>
+//     <p>Lets begin </p>
+//     <h2>so we mention it in a div tag</h2>
+//     </div>
+//     </>, 
+
+//     document.getElementById('root')
+// );
 
 ReactDOM.render(
     <>
-    <div>
-    <h1>Rithik CHoudhary</h1>
-    <p>Lets begin </p>
-    <h2>so we mention it in a div tag</h2>
-    </div>
-    </>, 
+    <h1>Netflix Series</h1>
+    <p>Here are the list of my fav Series</p>
 
+    <ol>
+        <li>Dark</li>
+        <li>Mr. RObot</li>
+        <li>Mirzapur</li>
+        <li>Sacred games</li>
+        <li>Utopia</li>
+        <li>Scam 1992</li>
+    </ol>
+    </>,
     document.getElementById('root')
-);
+)
