7f1a52fc240c6fcdef5892044033071513c0f187
burnt776 <burnt776@gmail.com>
2020-10-18 22:07:50 +0530

    nm

--- React JS/hello-world/src/index.js
+++ React JS/hello-world/src/index.js
@@ -1,5 +1,7 @@
 var React = require('react');
+
 var ReactDOM = require('react-dom');
 
 // ReactDOM.render('kya dikhana ha' , 'kaha dikhana ha' , 'callback function');
-ReactDOM.render( <h1> Hello World! </h1> , document.getElementById('root'));
+ReactDOM.render( <h1> Hello World! </h1> , 
+  document.getElementById('root'));
