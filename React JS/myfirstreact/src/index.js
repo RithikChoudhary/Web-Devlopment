8699f8e03400cf4d1797757c7810f394a30600b6
burnt776 <burnt776@gmail.com>
2020-10-19 00:10:37 +0530

    made cahnges

--- React JS/myfirstreact/src/index.js
+++ React JS/myfirstreact/src/index.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// ReactDOM.render(
+//     <div>
+//     <h1>Rithik CHoudhary</h1>
+//     <p>Lets begin </p>
+//     <h2>so we mention it in a div tag</h2>
+//     </div>, 
+
+//     document.getElementById('root')
+// );
+
+// we can use [] bracket to mention our tags
 ReactDOM.render(
-    <div>
-    <h1>Rithik CHoudhary</h1>
-    <p>Lets begin </p>
+    [
+    <h1>Rithik CHoudhary</h1>,
+    <p>Lets begin </p>,
     <h2>so we mention it in a div tag</h2>
-    </div>, 
+    ], 
     
     document.getElementById('root')
 );
-
  
