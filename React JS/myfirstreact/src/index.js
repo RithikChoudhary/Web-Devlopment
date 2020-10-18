678b2b196f2f7e0f2c3914a6c37866089850b9e8
burnt776 <burnt776@gmail.com>
2020-10-19 00:58:02 +0530

    template expression

--- React JS/myfirstreact/src/index.js
+++ React JS/myfirstreact/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const flname = 'Rithik Choudhary'
+const fname = 'Rithik'
+const lname = 'Choudhary'
+
+
 
 ReactDOM.render(
     <>
-        <h1>My name is {flname}</h1>
+        <h1>My name is {fname +' '+ lname}</h1>
+        d
+        <h1>My name is {`${fname} ${lname}`}</h1>
+        <h2>My name is {fname} {lname}</h2>
         <p>My lucky number is {10 + 2}</p>
         <p>generate random no {Math.random()}</p>
     </>,
