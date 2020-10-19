7adfa2f7136c3e7a6855247662404dcccd37f433
burnt776 <burnt776@gmail.com>
2020-10-19 23:46:45 +0530

    using date() func

--- React JS/myfirstreact/src/index.js
+++ React JS/myfirstreact/src/index.js
@@ -3,21 +3,22 @@ import ReactDOM from 'react-dom'
 
 const fname = 'Rithik'
 const lname = 'Choudhary'
-var x  = new Date();
-var hr = x.getHours()
-var min = x.getMinutes()
-var sec = x.getSeconds()
-var dat = x.getDate()
-var mon = x.getMonth()
-var year = x.getFullYear()
+var date  = new Date().toLocaleDateString();
+var time = new Date().toLocaleTimeString();
+// var hr = x.getHours()
+// var min = x.getMinutes()
+// var sec = x.getSeconds()
+// var dat = x.getDate()
+// var mon = x.getMonth()
+// var year = x.getFullYear()
 ReactDOM.render(
     <>
 
         <h1>My name is {fname +' '+ lname}</h1>
         <h1>My name is {`${fname} ${lname}`}</h1>
         <h2>My name is {fname} {lname}</h2>
-        <p>Current Time is {hr}:{min}:{sec}</p>
-        <p>Todays date is {dat}/{mon}/{year}</p>
+        <p>Current Time is {time}</p>
+        <p>Todays date is {date}</p>
 
         <p>My lucky number is {10 + 2}</p>
         <p>generate random no {Math.random()}</p>
