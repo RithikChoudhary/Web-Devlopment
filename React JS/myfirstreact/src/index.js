3c23096f0336743f9c2df6a8968a4b3cef33ac58
burnt776 <burnt776@gmail.com>
2020-10-20 00:22:30 +0530

    using image

--- React JS/myfirstreact/src/index.js
+++ React JS/myfirstreact/src/index.js
@@ -1,27 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const fname = 'Rithik'
-const lname = 'Choudhary'
-var date  = new Date().toLocaleDateString();
-var time = new Date().toLocaleTimeString();
-// var hr = x.getHours()
-// var min = x.getMinutes()
-// var sec = x.getSeconds()
-// var dat = x.getDate()
-// var mon = x.getMonth()
-// var year = x.getFullYear()
+
+const name = 'Rithik'
+const img1 = "https://picsum.photos/210/310"
+const img2 = "https://picsum.photos/220/320"
+const img3 = "https://picsum.photos/250/300"
 ReactDOM.render(
     <>
-
-        <h1>My name is {fname +' '+ lname}</h1>
-        <h1>My name is {`${fname} ${lname}`}</h1>
-        <h2>My name is {fname} {lname}</h2>
-        <p>Current Time is {time}</p>
-        <p>Todays date is {date}</p>
-
-        <p>My lucky number is {10 + 2}</p>
-        <p>generate random no {Math.random()}</p>
+        <h1 contentEditable='true'>Hey im {name}</h1>
+        <img src={img1} alt="Random img"/>
+        <img src={img2} alt="Random img"/>
+        <img src={img3} alt="Random img"/>
     </>,
     document.getElementById('root')
 )
