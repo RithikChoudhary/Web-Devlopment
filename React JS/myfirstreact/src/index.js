70c1f2a7c2d7380650750745dccfe771222c5fe3
burnt776 <burnt776@gmail.com>
2020-10-20 01:01:59 +0530

    css

--- React JS/myfirstreact/src/index.js
+++ React JS/myfirstreact/src/index.js
@@ -8,12 +8,12 @@ const img2 = "https://picsum.photos/220/320"
 const img3 = "https://picsum.photos/250/300"
 ReactDOM.render(
     <>
-        <h1 class="heading">Hey im {name}</h1>
-        <img src={img1} alt="Random img" />
-        <img src={img2} alt="Random img" />
-        <a href="#">
+        <h1 className="heading">Hey im {name}</h1>
+        <div className="img_div">
+            <img src={img1} alt="Random img" />
+            <img src={img2} alt="Random img" />
             <img src={img3} alt="Random img" />
-        </a>
+        </div>
     </>,
     document.getElementById('root')
 )
