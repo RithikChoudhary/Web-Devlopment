fcd6e93db8bd3a3ff215626ddf57ced8c16cc443
burnt776 <burnt776@gmail.com>
2020-10-20 00:51:40 +0530

    introducing css

--- React JS/myfirstreact/src/index.js
+++ React JS/myfirstreact/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import './index.css'
 
 const name = 'Rithik'
 const img1 = "https://picsum.photos/210/310"
@@ -8,7 +8,7 @@ const img2 = "https://picsum.photos/220/320"
 const img3 = "https://picsum.photos/250/300"
 ReactDOM.render(
     <>
-        <h1 contentEditable='true'>Hey im {name}</h1>
+        <h1 class="heading">Hey im {name}</h1>
         <img src={img1} alt="Random img" />
         <img src={img2} alt="Random img" />
         <a href="#">
