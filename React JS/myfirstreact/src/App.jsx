52add451b7b712c9f17e6722a64c4d2653f8630f
burnt776 <burnt776@gmail.com>
2020-10-21 01:46:48 +0530

    practice

--- React JS/myfirstreact/src/App.jsx
+++ React JS/myfirstreact/src/App.jsx
@@ -1,16 +1,35 @@
 import React from 'react'
-import Heading from './Heading'
-import Para from './Para'
-import List from './List'
+import './index.css'
 
-function App() {
+function App(){
+    let currDate = new Date(2020,10,21,14)
+    currDate = currDate.getHours()
+    let greeting = ''
+    const cssStyle = {}
+    if (currDate >= 1 && currDate <12){
+        greeting = 'Good Morning'
+        cssStyle.color = 'green'
+    }
+    else if(currDate >=12 && currDate <20)
+    {
+        greeting = 'Good Afternoon'
+        cssStyle.color = 'red'
+    }
+    else
+    {
+        greeting = 'good night'
+        cssStyle.color = 'blue'
+    }
     return (
         <>
-            <Heading />
-            <Para />
-            <List />
+        <div>
+            <h1 >
+                Hello sir, <span style ={cssStyle}>{greeting}</span>
+            </h1>
+        </div>
         </>
     )
+
 }
 
 export default App
