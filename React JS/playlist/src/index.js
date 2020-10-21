8f06cb4780afc30f990e84be1d5f0f069adc77a8
burnt776 <burnt776@gmail.com>
2020-10-22 02:26:15 +0530

    using props

--- React JS/playlist/src/index.js
+++ React JS/playlist/src/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <>
-    <div className='cards'>
-      <div className='card'>
-        <img src='https://wallpapercave.com/wp/wp4056410.jpg' alt='mypic' className='card_img' />
-        <div className='card_info'>
-          <span className='card_category'> A Netflix Orignal series</span>
-          <h3 className='card_title'> Dark </h3>
-          <a href='https://www.netflix.com/in/title/80990668?source=35' target='_blank'>
-            <button>Watch Now</button>
-          </a>
+function Card() {
+  return (
+    <>
+      <div className='cards'>
+        <div className='card'>
+          <img src='https://wallpapercave.com/wp/wp4056410.jpg' alt='mypic' className='card_img' />
+          <div className='card_info'>
+            <span className='card_category'> A Netflix Orignal series</span>
+            <h3 className='card_title'> Dark </h3>
+            <a href='https://www.netflix.com/in/title/80990668?source=35' target='_blank'>
+              <button>Watch Now</button>
+            </a>
+          </div>
         </div>
-      </div>
 
-    </div>
-  </>,
+      </div>
+    </>
+  )
+}
+ReactDOM.render(<Card />
+  ,
   document.getElementById('root'))
