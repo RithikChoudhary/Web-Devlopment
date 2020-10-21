44a1eebc32244331a9d7e6b543c0b5edf51b7344
burnt776 <burnt776@gmail.com>
2020-10-22 02:18:41 +0530

    deleted some files

--- React JS/playlist/src/index.js
+++ React JS/playlist/src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<>
-</>,
-document.getElementById('root'))
+ReactDOM.render(
+  <>
+    <div className='cards'>
+      <div className='card'>
+        <img src='https://wallpapercave.com/wp/wp4056410.jpg' alt='mypic' className='card_img' />
+        <div className='card_info'>
+          <span className='card_category'> A Netflix Orignal series</span>
+          <h3 className='card_title'> Dark </h3>
+          <a href='https://www.netflix.com/in/title/80990668?source=35' target='_blank'>
+            <button>Watch Now</button>
+          </a>
+        </div>
+      </div>
+
+    </div>
+  </>,
+  document.getElementById('root'))
