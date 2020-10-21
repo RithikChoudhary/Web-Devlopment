ef2ed19d4a9871a6bb25435e228fd12ab160f1c3
burnt776 <burnt776@gmail.com>
2020-10-22 01:40:35 +0530

    random

--- React JS/myfirstreact/src/App.jsx
+++ React JS/myfirstreact/src/App.jsx
@@ -1,32 +1,12 @@
 import React from 'react'
 import './index.css'
+import add from './Calc'
 
 function App() {
-    let currDate = new Date(2020, 10, 21, 14)
-    currDate = currDate.getHours()
-    let greeting = ''
-    const cssStyle = {}
-    if (currDate >= 1 && currDate < 12) {
-        greeting = 'Good Morning'
-        cssStyle.color = 'green'
-    }
-    else if (currDate >= 12 && currDate < 20) {
-        greeting = 'Good Afternoon'
-        cssStyle.color = 'red'
-    }
-    else {
-        greeting = 'good night'
-        cssStyle.color = 'blue'
-    }
     return (
         <>
-            <div>
-                <h1 >
-                    Hello sir, <span style={cssStyle}>{greeting}</span>
-                </h1>
-            </div>
-        </>
-    )
+            <li>{add(40, 4)}</li>
+        </>)
 
 }
 
--- React JS/myfirstreact/src/index.js
+++ React JS/myfirstreact/src/index.js
@@ -1,9 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import add from './Calc'
-ReactDOM.render(
-    <>
-    <li>{add(40,4)}</li>
-    </>,
-    document.getElementById('root')
+import App from './App'
+ReactDOM.render( <App />, document.getElementById('root')
 )
