88c2219cb7164206fdc9d4a7e804ac975e280612
burnt776 <burnt776@gmail.com>
2020-10-22 00:51:59 +0530

    use npm install to fix

--- React JS/myfirstreact/src/App.jsx
+++ React JS/myfirstreact/src/App.jsx
@@ -1,32 +1,30 @@
 import React from 'react'
 import './index.css'
 
-function App(){
-    let currDate = new Date(2020,10,21,14)
+function App() {
+    let currDate = new Date(2020, 10, 21, 14)
     currDate = currDate.getHours()
     let greeting = ''
     const cssStyle = {}
-    if (currDate >= 1 && currDate <12){
+    if (currDate >= 1 && currDate < 12) {
         greeting = 'Good Morning'
         cssStyle.color = 'green'
     }
-    else if(currDate >=12 && currDate <20)
-    {
+    else if (currDate >= 12 && currDate < 20) {
         greeting = 'Good Afternoon'
         cssStyle.color = 'red'
     }
-    else
-    {
+    else {
         greeting = 'good night'
         cssStyle.color = 'blue'
     }
     return (
         <>
-        <div>
-            <h1 >
-                Hello sir, <span style ={cssStyle}>{greeting}</span>
-            </h1>
-        </div>
+            <div>
+                <h1 >
+                    Hello sir, <span style={cssStyle}>{greeting}</span>
+                </h1>
+            </div>
         </>
     )
 
