5f262f40b4e8c8d121fe62d7b05084802e34f8bd
burnt776 <burnt776@gmail.com>
2020-10-25 01:50:58 +0530

    making diffrent component

---
+++ React JS/playlist/src/SlotMach.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+const SlotM = (props) => {
+    // let x = '😄'
+    // let y = '😄'
+    // let z = '😄'
+    let x = props.x
+    let y = props.y
+    let z = props.z
+
+    if ((x === y) && (y === z)) {
+        return (
+            <>
+                <div className='slot_inner'>
+                    <h1>{x} {y} {z}</h1>
+                    <h1>This is matching</h1>
+                    <hr />
+                </div>
+            </>
+        )
+    }
+    else {
+        return (
+            <>
+                <div className='slot_inner'>
+                    <h1>{x} {y} {z}</h1>
+                    <h1>This is Not matching</h1>
+                    <hr />
+                </div>
+            </>
+        )
+    }
+}
+
+export default SlotM
