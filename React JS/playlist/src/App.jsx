096dfbbe9c8e3954b6bda06b95d54985f793c4af
burnt776 <burnt776@gmail.com>
2020-10-26 00:47:15 +0530

    on click and hooks

--- React JS/playlist/src/App.jsx
+++ React JS/playlist/src/App.jsx
@@ -1,23 +1,41 @@
 import React from 'react';
-import SlotM from './SlotMach'
+import { useState } from 'react';
+// import SlotM from './SlotMach'
+
+
+// const App = () => {
+//     return (
+//         <>
+//             <h1 className='heading_style'>
+//                 🎰 Welcome to <span style={{ fontWeight: 'bold' }}>Slot machine game </span>🎰
+//             </h1>
+//             <div className='slotmachine'>
+//                 <SlotM x='😄' y='😄' z='😄' />
+//                 <hr/>
+//                 <SlotM x='😄' y='⛈' z='😄' />
+//                 <hr/>
+//                 <SlotM x='🔥' y='〽' z='😄' />
+//                 <hr/>
+//             </div>
+//         </>
+//     )
+// }
+
 
 
 const App = () => {
+    const state = useState();
+    const [count, setCount] = useState(0)
+
+    const IncNum = () => {
+        setCount(count + 1);
+        console.log('click')
+    }
     return (
         <>
-            <h1 className='heading_style'>
-                🎰 Welcome to <span style={{ fontWeight: 'bold' }}>Slot machine game </span>🎰
-            </h1>
-            <div className='slotmachine'>
-                <SlotM x='😄' y='😄' z='😄' />
-                <hr/>
-                <SlotM x='😄' y='⛈' z='😄' />
-                <hr/>
-                <SlotM x='🔥' y='〽' z='😄' />
-                <hr/>
-            </div>
+            <h1 className='h1'> {count} </h1>
+            <button className='b' onClick={IncNum}>Click Me</button>
         </>
     )
 }
-
 export default App;
