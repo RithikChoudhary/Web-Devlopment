6e069f8c88ec1a0f0a7ae7a98a5ac5000120f680
burnt776 <burnt776@gmail.com>
2020-10-28 21:52:35 +0530

    created the form

--- React JS/playlist/src/App.jsx
+++ React JS/playlist/src/App.jsx
@@ -1,33 +1,15 @@
 import React from 'react'
-import { useState } from 'react'
-
 
 const App = () => {
-
-    const purple = "#8e44ad"
-    const [bg, setbg] = useState(purple);
-    const [name,setName] = useState("click Me")
-
-    const bgChange = () => {
-        let newbg = "#34495e"
-        setbg(newbg)
-        setName("Ouch!!")
-    }
-
-    const doubl = () =>{
-        setbg(purple)
-        setName("Double clicked")
-    }
-
-
     return (
         <>
-            <div style={{ backgroundColor: bg }}>
-                <button onMouseEnter={bgChange} onMouseLeave= {doubl}>{name}</button>
+            <div>
+                <h1>Hello</h1>
+                <input type="text" placeholder="Enter your text"/>
+                <button>Click me</button>
             </div>
         </>
     )
-    
 }
 
 export default App;
