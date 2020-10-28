454df9d12c116259db7d1960aaf37afcc86ff293
burnt776 <burnt776@gmail.com>
2020-10-28 22:52:10 +0530

    used onChnage and event handling

--- React JS/playlist/src/App.jsx
+++ React JS/playlist/src/App.jsx
@@ -3,21 +3,25 @@ import { useState } from 'react'
 
 const App = () => {
 
-    const [name,setName] = useState("rithik")
-    const inputEvent = ()=>{
-        // console.log(event.target.value);
+    const [name, setName] = useState()
+    const [fullName, setFullName] = useState()
+    
+    const inputEvent = (event) => {
         setName(event.target.value)
     }
-   
+    const onSubmit = () => {
+        setFullName(name)
+    }
+
     return (
         <>
             <div>
-                <h1>Hello {name}</h1>
+                <h1>Hello {fullName}</h1>
                 <input type="text" placeholder="Enter your text"
                     onChange={inputEvent}
-                    value=""
+
                 />
-                <button>Click me</button>
+                <button onClick={onSubmit}>Click me</button>
             </div>
         </>
     )
