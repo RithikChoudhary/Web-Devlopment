dfb4851c7c1d311eafa44eeb417ea0754c2ba03d
burnt776 <burnt776@gmail.com>
2020-10-28 20:31:33 +0530

    trying to merge pull request

--- React JS/playlist/src/App.jsx
+++ React JS/playlist/src/App.jsx
@@ -1,26 +1,5 @@
 import React from 'react';
 import { useState } from 'react';
-// import SlotM from './SlotMach'
-
-
-// const App = () => {
-//     return (
-//         <>
-//             <h1 className='heading_style'>
-//                 🎰 Welcome to <span style={{ fontWeight: 'bold' }}>Slot machine game </span>🎰
-//             </h1>
-//             <div className='slotmachine'>
-//                 <SlotM x='😄' y='😄' z='😄' />
-//                 <hr/>
-//                 <SlotM x='😄' y='⛈' z='😄' />
-//                 <hr/>
-//                 <SlotM x='🔥' y='〽' z='😄' />
-//                 <hr/>
-//             </div>
-//         </>
-//     )
-// }
-
 
 
 const App = () => {
