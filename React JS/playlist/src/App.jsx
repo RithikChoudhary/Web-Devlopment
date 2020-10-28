1f8ca72662c3be835b2b3cade2fee3805862ec84
burnt776 <burnt776@gmail.com>
2020-10-28 22:41:35 +0530

    using onChange value with hooks

--- React JS/playlist/src/App.jsx
+++ React JS/playlist/src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
+
+    const [name,setName] = useState("rithik")
+    const inputEvent = ()=>{
+        // console.log(event.target.value);
+        setName(event.target.value)
+    }
+   
     return (
         <>
             <div>
-                <h1>Hello</h1>
-                <input type="text" placeholder="Enter your text"/>
+                <h1>Hello {name}</h1>
+                <input type="text" placeholder="Enter your text"
+                    onChange={inputEvent}
+                    value=""
+                />
                 <button>Click me</button>
             </div>
         </>
