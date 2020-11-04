aa394be394557d6dca76fc0f86e383701501e349
burnt776 <burnt776@gmail.com>
2020-11-05 01:30:46 +0530

    done adding into list

--- React JS/todo/src/App.jsx
+++ React JS/todo/src/App.jsx
@@ -2,23 +2,41 @@ import React, { useState } from 'react'
 
 const App = () => {
 
-    const [inputList,setinputList] = useState()
+    const [inputList, setinputList] = useState()
+    const [items, setItems] = useState([])
+
+    const itemEvent = (event) => {
+        setinputList(event.target.value)
+    }
+    const listOfItems = () => {
+        setItems((olditems) => {
+            return [...olditems, inputList]
+        })
+        setinputList('')
+    }
+
     return (
         <>
-        <div className="main_div">
-            <div className="center_div">
-                <br/>
-                <h1>ToDo List</h1>
-                <br/>
-                <input type="text" placeholder="Add a item" onChange={itemEvent}/>
+            <div className="main_div">
+                <div className="center_div">
+                    <br />
+                    <h1>ToDo List</h1>
+                    <br />
+                    <input type="text" placeholder="Add a item"
+                        value={inputList}
+                        onChange={itemEvent}
+                    />
 
-                <button> + </button>
+                    <button onClick={listOfItems}> + </button>
 
-                <ol>
-                    <li>buy apple</li>
-                </ol>
+                    <ol>
+                        {items.map((itemval) => {
+                            return (<li>{itemval}</li>)
+                        })}
+
+                    </ol>
+                </div>
             </div>
-        </div>
         </>
     )
 };
